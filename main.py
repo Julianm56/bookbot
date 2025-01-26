c5171c25265b1ab06d4e1a931ced12f1994c131d
Julianm56 <jpimientarm56@gmail.com>
2025-01-25 20:16:52 -0700

    Added character count

--- main.py
+++ main.py
@@ -7,12 +7,28 @@ def word_count_book(text):
     words = text.split()
     return len(words) 
 
+def character_count(text):
+    characters = {}
+    lower_case_text = text.lower()
+
+    for word in lower_case_text:
+        if word in characters:
+            characters[word]+=1
+        else:
+            characters[word] = 1
+
+    return characters
+        
+
 def main():
     content = open_file("books/frankenstein.txt")
     print(content)
 
     num_words = word_count_book(content)
     print(num_words)
+
+    chars = character_count(content)
+    print(chars)
 
 
 main()
